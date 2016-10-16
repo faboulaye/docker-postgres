17d4c02e4772067b145f1c8665183da8a879a519
Firdaws Aboulaye <aboufred@gmail.com>
2016-10-16 16:48:32 +0200

    perform container

--- debian/docker-entrypoint.sh
+++ debian/docker-entrypoint.sh
@@ -5,14 +5,14 @@ initdb -D $PGDATA
 echo "host all  all    0.0.0.0/0  md5" >> $PGDATA/pg_hba.conf
 echo "listen_addresses='*'" >> $PGDATA/postgresql.conf
 
-pg_ctl -D $PGDATA -l var/log/postgresql/postgresql-$PG_VERSION-main.log start
+pg_ctl -D $PGDATA -l var/log/postgresql/postgresql-$PG_VERSION-main.log -w start
 
 if [ "$PG_DB" != 'postgres' ]; then
   if psql -lqt | cut -d \| -f 1 | grep -qw $PG_DB; then
     echo "Database already exist !!!"
   else
-    createSql="CREATE DATABASE $PG_DB ENCODING $PG_ENCODING;"
-    echo $createSql | postgres --single -jE
+    createSql="CREATE DATABASE $PG_DB"
+    echo $createSql | psql postgres
     echo
   fi
 fi
@@ -25,7 +25,7 @@ fi
 
 userSql="$op USER $PG_USER WITH SUPERUSER PASSWORD '$PG_PASSWORD'; \
   GRANT ALL PRIVILEGES ON DATABASE $PG_DB TO $PG_USER;"
-echo $userSql | postgres --single -jE
+echo $userSql | psql postgres
 
 echo
 for f in $INITDB/*; do
@@ -38,7 +38,5 @@ for f in $INITDB/*; do
     echo
 done
 
-pg_ctl -D "$PGDATA" -m fast -w stop
-
 echo 'PostgreSQL init process complete; ready for start up.'
-postgres
+psql postgres
